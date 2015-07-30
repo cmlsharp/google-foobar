1ec3934dfa9bb4857a1be5c9bfa0947212d54317
Chad Sharp <crossroads1112@gmail.com>
2015-07-30 19:05:04 -0400

    Added comments

--- 3-2_when_it_rains_it_pours.py
+++ 3-2_when_it_rains_it_pours.py
@@ -60,8 +60,50 @@ Output:
 
 
 def answer(heights):
+    # Right off of the bat, water can't collect if there are fewer than 3
+    # hutches
     if len(heights) < 3:
         return 0
+    """
+    When figuring out how to go about this program, I thought about how to
+    qualify where exactly water would collect. It would collect between two
+    columns assuming there doesn't exist any larger columns in between them. The
+    highest the water level could possibly be is the height of the shortest
+    bounding column. I'd call these columns towers. This concept was simple
+    enough  The problem then arises of figuring out how to programatically
+    define these towers and how best to find them.
+
+    My initial algorithm started from the first column and would iterate through
+    each column, looking for a column that was its height or taller. If it found
+    one, it would add that column to the list of towers and the new tower would
+    become the new standard. This worked great in certain cases, but consider
+    the following configuration:
+
+    X
+    XX X
+    XXXX
+    3212
+
+    My algorithm would see the first tower with a height of three and would not
+    find any other columns with a height of 3 or greater. Thus it would say no
+    water would accumulate and would return a value of 0.
+
+    Well that's no good. What then?
+    I kept thinking, to a human brain, it's so obvious. We can see the whole
+    picture so it's easy to see the boundries between which water would
+    accumulate. That's when I realized the problem with my algorithm. It only
+    saw the picture from left to right.
+
+    I added another iteration, from right to left, creating two different lists
+    of towers. There was some overlap so I converted both lists to sets (set theory
+    WOOOO!!).
+
+    Next I iterate through each tower and determine the water level by figuring
+    out which is shorter, it or the following tower. Then I iterate through each
+    intermediate column and add the subtraction of its height from the water
+    level to the total score.
+
+    """
     primtowers = [(0, heights[0])]
     ultowers = [(len(heights) - 1, heights[-1])]
     for primcolumn in range(len(heights) - 1):
